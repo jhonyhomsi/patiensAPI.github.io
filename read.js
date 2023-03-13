bb190023147b18e83b95fc5fbcb177aa3096ef12
Jhony10-bit <johnny.aj.antonio@gmail.com>
2023-03-13 10:04:50 +0200

    API Update

---
+++ read.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const XLSX = require('xlsx');
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+
+app.use(express.urlencoded({ extended: true }));
+
+// Requiring the module
+const reader = require('xlsx')
+
+// Reading our test file
+const file = reader.readFile('exam-results.xlsx')
+
+let data = []
+
+const sheets = file.SheetNames
+
+for(let i = 0; i < sheets.length; i++)
+{
+const temp = reader.utils.sheet_to_json(
+		file.Sheets[file.SheetNames[i]])
+temp.forEach((res) => {
+	data.push(res)
+})
+}
+
+// Printing data
+console.log(data)
+  res.send(data);
+});
+
+app.listen(8000, () => {
+  console.log('Server listening on port 8000');
+})
